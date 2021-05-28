aedd9b39069f890d1d41c96d0a42d407ef1ff366
Eemeli Ranta <rantaeemeli@gmail.com>
2021-05-29 01:01:06 +0300

    Add methods to get crypto price in fiat

--- crypto_fifo_taxes/models/currency.py
+++ crypto_fifo_taxes/models/currency.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.utils.translation import gettext as _
 
@@ -5,7 +7,10 @@ from crypto_fifo_taxes.utils.models import TransactionDecimalField
 
 
 class Currency(models.Model):
-    """eg. "BTC"""
+    """
+    Basic information about a currency.
+    e.g. `BTC`
+    """
 
     symbol = models.CharField(
         max_length=30,
@@ -26,9 +31,50 @@ class Currency(models.Model):
         verbose_name=_("Is FIAT"),
     )
 
+    def get_fiat_price(self, date=None, fiat=None) -> "CurrencyPrice":
+        """
+        Get the FIAT price for a crypto on a specific date.
+
+        If no `fiat` currency is defined, settings.DEFAULT_FIAT_CURRENCY will be used instead.
+        Fetch price for the crypto if no record for entered fiat and date is found.
+        """
+        if date is None:
+            raise TypeError("Date must be entered!")
+
+        if isinstance(date, datetime):
+            date = date.date()
+
+        if self.is_fiat is False:
+            raise TypeError("")
+
+        currency_price = None
+        # Get crypto price in entered FIAT currency
+        if fiat is not None:
+            assert isinstance(fiat, Currency) and fiat.is_fiat is True
+            currency_price = self.prices.filter(date=date, fiat=fiat).first()
+
+        # Price was not found for entered FIAT
+        if currency_price is None:
+            from crypto_fifo_taxes.utils.currency import get_default_fiat
+
+            fiat = get_default_fiat()
+            currency_price = self.prices.filter(date=date, fiat=fiat).first()
+            if fiat is not None:
+                pass  # TODO: Create price for missed FIAT by converting from default currency
+
+        # Price was not found even in default currency
+        if currency_price is None:
+            pass  # TODO: Fetch coin price from an API
+
+        return currency_price
+
 
 class CurrencyPair(models.Model):
-    """eg. BTCUSDT"""
+    """
+    A trading pair between two currencies
+    First symbol is the currency being bought, second is the currency used to buy.
+    e.g. the pair `BTCUSDT`. BTC is bought using USDT
+    """
 
     symbol = models.CharField(max_length=30)
     buy = models.ForeignKey(
@@ -44,10 +90,7 @@ class CurrencyPair(models.Model):
 
 
 class CurrencyPrice(models.Model):
-    """
-    Crypto price in FIAT on a specific date
-    BASE_CRYPTO_FIAT_CURRENCY will be used as the base for all Crypto currency values
-    """
+    """Crypto price in FIAT on a specific date"""
 
     currency = models.ForeignKey(
         to=Currency,
@@ -67,7 +110,7 @@ class CurrencyPrice(models.Model):
     volume = TransactionDecimalField()
 
     class Meta:
-        # A price can only have one price per day
+        # A price can only have one price per day per FIAT currency
         unique_together = (
             "currency",
             "date",
