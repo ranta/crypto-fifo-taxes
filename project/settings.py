dc1cfe9bc7d15663b9ad70b225366d922fe5d4a2
Eemeli Ranta <rantaeemeli@gmail.com>
2021-06-04 15:45:34 +0300

    Remove duplicate `STATIC_URL` from `settings.py`

--- project/settings.py
+++ project/settings.py
@@ -93,10 +93,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
