7a23c10c3260c73bfea2008b3cc7e7ada448a133
Eemeli Ranta <rantaeemeli@gmail.com>
2021-06-14 01:33:33 +0300

    Fix `get_default_fiat` keyword typo

--- crypto_fifo_taxes/utils/currency.py
+++ crypto_fifo_taxes/utils/currency.py
@@ -8,4 +8,4 @@ from crypto_fifo_taxes.models import Currency
 @lru_cache()
 def get_default_fiat() -> Currency:
     """Return the currency object for default fiat currency"""
-    return Currency.objects.get(is_fiat=True, symboget_fiat_currencyl=settings.DEFAULT_FIAT_CURRENCY)
+    return Currency.objects.get(is_fiat=True, symbol=settings.DEFAULT_FIAT_CURRENCY)
