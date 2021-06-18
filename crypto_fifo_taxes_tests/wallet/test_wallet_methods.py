1818ec244359d816ca959b6d7708fd08f0de37a9
Eemeli Ranta <rantaeemeli@gmail.com>
2021-06-19 00:21:59 +0300

    Add a method to get wallet current balance

--- crypto_fifo_taxes_tests/wallet/test_wallet_methods.py
+++ crypto_fifo_taxes_tests/wallet/test_wallet_methods.py
@@ -1,17 +1,38 @@
+from decimal import Decimal
+
 import pytest
 
 from crypto_fifo_taxes.models import Currency
-from crypto_fifo_taxes_tests import factories
+from crypto_fifo_taxes_tests.factories import TransactionDetailFactory, TransactionFactory, WalletFactory
 
 
 @pytest.mark.django_db
-def test_wallet_get_currencies():
-    wallet = factories.WalletFactory.create()
+def test_wallet_get_used_currency_ids():
+    wallet = WalletFactory.create()
 
-    # Create a deposit
-    for i in range(0, 10):
-        factories.TransactionFactory.create(to_detail=factories.TransactionDetailFactory.create(wallet=wallet))
+    # Create deposits
+    TransactionFactory.create(to_detail=TransactionDetailFactory.create(wallet=wallet, currency="BTC"))
+    for i in range(0, 9):
+        TransactionFactory.create(to_detail=TransactionDetailFactory.create(wallet=wallet))
 
-    currencies = wallet.get_currencies()
+    currencies = wallet.get_used_currency_ids()
     assert len(currencies) == 10
     assert Currency.objects.last().pk in currencies
+    assert Currency.objects.get(symbol="BTC").pk in currencies
+
+
+@pytest.mark.django_db
+def test_wallet_get_balance_deposit_and_withdrawal():
+    wallet = WalletFactory.create()
+
+    # Create deposits
+    TransactionFactory.create(to_detail=TransactionDetailFactory.create(wallet=wallet, currency="BTC", quantity=5))
+    TransactionFactory.create(to_detail=TransactionDetailFactory.create(wallet=wallet, currency="BTC", quantity=10))
+    TransactionFactory.create(from_detail=TransactionDetailFactory.create(wallet=wallet, currency="BTC", quantity=2.5))
+
+    currencies = wallet.get_balance()
+    assert currencies.count() == 1
+    assert currencies.first().symbol == "BTC"
+    assert currencies.first().deposits == Decimal(15)
+    assert currencies.first().withdrawals == Decimal(2.5)
+    assert currencies.first().balance == Decimal(12.5)
