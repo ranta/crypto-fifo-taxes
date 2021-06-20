64c23a619092b99f9562f360962791a77a02f5d7
Eemeli Ranta <rantaeemeli@gmail.com>
2021-06-20 19:12:56 +0300

    Calculate Crypto -> FIAT trade `to_detail` `cost_basis`

--- crypto_fifo_taxes/models/transaction.py
+++ crypto_fifo_taxes/models/transaction.py
@@ -22,17 +22,28 @@ class Transaction(models.Model):
     gain = TransactionDecimalField(null=True)  # Calculated field
     fee_amount = TransactionDecimalField(null=True)  # Calculated field
 
-
     def _handle_buy_crypto_with_fiat_cost_basis(self):
         self.from_detail.cost_basis = self.from_detail.quantity
 
         # Simply distribute FIAT spent to crypto bought
         self.to_detail.cost_basis = self.from_detail.quantity / self.to_detail.quantity
 
+    def _handle_sell_crypto_to_fiat_cost_basis(self):
+        # TODO: Use FIFO to find exactly which coins were used
+        self.from_detail.cost_basis = None
+
+        # Use sold price as cost basis
+        self.to_detail.cost_basis = self.to_detail.quantity / self.from_detail.quantity
+
     def fill_cost_basis(self):
         # TODO: Use `deemed acquisition cost` (hankintameno-olettama) when applicable
+        # Buy Crypto with FIAT
         if self.from_detail.currency.is_fiat is True and self.to_detail.currency.is_fiat is False:
             self._handle_buy_crypto_with_fiat_cost_basis()
+
+        # Sell Crypto to FIAT
+        if self.from_detail.currency.is_fiat is False and self.to_detail.currency.is_fiat is True:
+            self._handle_sell_crypto_to_fiat_cost_basis()
 
 
 class TransactionDetail(models.Model):
--- crypto_fifo_taxes_tests/transaction/test_cost_basis.py
+++ crypto_fifo_taxes_tests/transaction/test_cost_basis.py
@@ -1,35 +1,35 @@
-from datetime import date, datetime
+from datetime import datetime, timedelta
 from decimal import Decimal
 
 import pytest
 import pytz
 
-from crypto_fifo_taxes.models import CurrencyPrice
 from crypto_fifo_taxes_tests.factories import CryptoCurrencyFactory, FiatCurrencyFactory, WalletFactory
-from crypto_fifo_taxes_tests.factories.currency import create_currency_price_history
 from crypto_fifo_taxes_tests.factories.utils import WalletHelper
 
 
 @pytest.mark.django_db
-def test_calc_cost_basis():
+def test_calc_simple_fiat_trades_cost_basis():
     wallet = WalletFactory.create()
     wallet_helper = WalletHelper(wallet)
 
     crypto = CryptoCurrencyFactory.create(symbol="BTC")
     fiat = FiatCurrencyFactory.create(symbol="EUR")
 
-    create_currency_price_history(crypto, fiat, start_date=date(2010, 1, 1))
-    # Price on day 0
-    assert CurrencyPrice.objects.get(currency=crypto, fiat=fiat, date=date(2010, 1, 1)).price == Decimal(1000)
-    # Price on last day, after 30 days of increase
-    assert CurrencyPrice.objects.get(currency=crypto, fiat=fiat, date=date(2010, 1, 31)).price == Decimal(1300)
-
     # Deposit FIAT to wallet
     tx_time = datetime(2010, 1, 1, 12, 0, 0, tzinfo=pytz.UTC)  # 12:00 on day 0
     wallet_helper.deposit(fiat, 1000, timestamp=tx_time)
 
     # Buy 10 BTC with 1000 EUR
-    tx = wallet_helper.buy_crypto(crypto, 10, fiat, 1000, timestamp=tx_time)
+    tx = wallet_helper.buy_crypto(crypto, 10, fiat, 1000, tx_time + timedelta(hours=1))
+    assert tx.to_detail.cost_basis == Decimal(100)  # 1 BTC == 100 EUR
 
-    # 1 BTC == 100 EUR
-    assert tx.to_detail.cost_basis == Decimal(100)
+    # Sell 5 BTC for 1000 EUR
+    tx = wallet_helper.sell_crypto(crypto, 5, fiat, 1000, tx_time + timedelta(days=2))
+    assert tx.to_detail.cost_basis == Decimal(200)  # 1 BTC == 200 EUR
+
+    # Sell 5 BTC for 2000 EUR
+    tx = wallet_helper.sell_crypto(crypto, 5, fiat, 2000, tx_time + timedelta(days=3))
+    assert tx.to_detail.cost_basis == Decimal(400)  # 1 BTC == 400 EUR
+
+
