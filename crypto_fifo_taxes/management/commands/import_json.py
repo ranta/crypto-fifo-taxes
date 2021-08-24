85d3a3445965d2c218cad5f72b9096f4fd7a610f
Eemeli Ranta <rantaeemeli@gmail.com>
2021-08-24 23:26:33 +0300

    Allow importing fees with json file importer

--- crypto_fifo_taxes/management/commands/import_json.py
+++ crypto_fifo_taxes/management/commands/import_json.py
@@ -1,13 +1,14 @@
 import json
 import os
 from decimal import Decimal
+from typing import Optional
 
 from django.conf import settings
 from django.core.management import BaseCommand
 from django.db.transaction import atomic
 
 from crypto_fifo_taxes.enums import TransactionType
-from crypto_fifo_taxes.models import Wallet
+from crypto_fifo_taxes.models import Transaction, Wallet
 from crypto_fifo_taxes.utils.binance.binance_api import bstrptime
 from crypto_fifo_taxes.utils.currency import get_or_create_currency
 from crypto_fifo_taxes.utils.transaction_creator import TransactionCreator
@@ -17,11 +18,18 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--file", type=str)
 
-    def get_wallets(self, row: dict) -> tuple[Wallet, Wallet]:
+    def get_wallets(self, row: dict) -> tuple[Wallet, Wallet, Optional[Wallet]]:
         if "from_wallet" in row and "to_wallet" in row:
-            return Wallet.objects.get(name=row["from_wallet"]), Wallet.objects.get(name=row["to_wallet"])
+            if "fee_wallet" in row:
+                return (
+                    Wallet.objects.get(name=row["from_wallet"]),
+                    Wallet.objects.get(name=row["to_wallet"]),
+                    Wallet.objects.get(name=row["fee_wallet"]),
+                )
+            else:
+                return Wallet.objects.get(name=row["from_wallet"]), Wallet.objects.get(name=row["to_wallet"]), None
         wallet = Wallet.objects.get(name=row["wallet"])
-        return wallet, wallet
+        return wallet, wallet, wallet
 
     def handle_imported_rows(self, data):
         for row in data:
@@ -36,22 +44,32 @@ class Command(BaseCommand):
                 tx_creator.add_from_detail(
                     wallet=wallets[0],
                     currency=get_or_create_currency(row["from_symbol"]),
-                    quantity=Decimal(row["from_amount"]),
+                    quantity=Decimal(str(row["from_amount"])),
                 )
             if "to_symbol" in row:
                 tx_creator.add_to_detail(
                     wallet=wallets[1],
                     currency=get_or_create_currency(row["to_symbol"]),
-                    quantity=Decimal(row["to_amount"]),
+                    quantity=Decimal(str(row["to_amount"])),
+                )
+            if "fee_symbol" in row:
+                tx_creator.add_to_detail(
+                    wallet=wallets[2],
+                    currency=get_or_create_currency(row["fee_symbol"]),
+                    quantity=Decimal(str(row["fee_amount"])),
                 )
 
             tx_creator.create_transaction()
 
     @atomic
     def handle(self, *args, **kwargs):
-        filename = kwargs.pop("file", "import.json")
+        transactions_count = Transaction.objects.count()
+
+        filename = kwargs.pop("file") or "import.json"
         filepath = os.path.join(settings.BASE_DIR, "app", filename)
 
         with open(filepath) as json_file:
             data = json.load(json_file)
             self.handle_imported_rows(data)
+
+        print(f"New transactions created: {Transaction.objects.count() - transactions_count}")
--- crypto_fifo_taxes_tests/transaction/test_fees.py
+++ crypto_fifo_taxes_tests/transaction/test_fees.py
@@ -14,7 +14,7 @@ from crypto_fifo_taxes_tests.utils import WalletHelper
 
 
 @pytest.mark.django_db
-def test_fees_simple():
+def test_fees_paid_with_new_currency():
     fiat = FiatCurrencyFactory.create(symbol="EUR")
     crypto = CryptoCurrencyFactory.create(symbol="BTC")
 
@@ -40,6 +40,27 @@ def test_fees_simple():
     wallet_helper.trade(crypto, Decimal("9.99"), fiat, 999, fiat, 1)
     assert wallet.get_current_balance("BTC") == Decimal(0)
     assert wallet.get_current_balance("EUR") == Decimal(998)
+
+
+@pytest.mark.django_db
+def test_fees_paid_with_original_currency():
+    fiat = FiatCurrencyFactory.create(symbol="EUR")
+    crypto = CryptoCurrencyFactory.create(symbol="BTC")
+
+    wallet = WalletFactory.create(fiat=fiat)
+    wallet_helper = WalletHelper(wallet)
+
+    # Deposit FIAT to wallet
+    wallet_helper.deposit(fiat, 1000)
+
+    # Buy 10 BTC with 1000 EUR
+    tx = wallet_helper.trade(fiat, 950, crypto, 10, fiat, 50)
+    assert tx.to_detail.cost_basis == Decimal(95)  # 1 BTC == 100 EUR
+    assert tx.fee_detail.cost_basis == Decimal(1)
+    assert tx.fee_detail.quantity == 50
+    assert tx.fee_amount == 50
+    assert wallet.get_current_balance("BTC") == 10
+    assert wallet.get_current_balance("EUR") == 0
 
 
 @pytest.mark.django_db
@@ -92,3 +113,27 @@ def test_fee_withdrawal():
         quantity=Decimal(10),
     )
     assert wallet.get_current_balance("BTC") == Decimal(0)
+
+
+@pytest.mark.django_db
+def test_fee_transfer():
+    fiat = FiatCurrencyFactory.create(symbol="EUR")
+    crypto = CryptoCurrencyFactory.create(symbol="BTC")
+
+    wallet_from = WalletFactory.create(fiat=fiat)
+    wallet_to = WalletFactory.create(fiat=fiat)
+    wallet_helper = WalletHelper(wallet_from)
+
+    wallet_helper.deposit(fiat, 1000)
+    wallet_helper.trade(fiat, 1000, crypto, 10)
+
+    # Fees should be removed from the `withdrawn amount` instead of from wallet
+    tx_creator = TransactionCreator(timestamp=wallet_helper.tx_time.timestamp)
+    tx_creator.add_from_detail(wallet=wallet_from, currency=crypto, quantity=10)
+    tx_creator.add_to_detail(wallet=wallet_to, currency=crypto, quantity=10)
+
+    # Fee should be directed to the `TO` wallet when transferring funds.
+    tx_creator.add_fee_detail(wallet=wallet_to, currency=crypto, quantity=1)
+    tx_creator.create_transfer()
+    assert wallet_from.get_current_balance("BTC") == Decimal(0)
+    assert wallet_to.get_current_balance("BTC") == Decimal(9)
