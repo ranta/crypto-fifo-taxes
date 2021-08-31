6560c8f5d4892727679bb3976df91de42a771bb8
Eemeli Ranta <rantaeemeli@gmail.com>
2021-08-31 19:18:37 +0300

    Improve swap transaction test

--- crypto_fifo_taxes_tests/transaction/test_transfer_and_swap.py
+++ crypto_fifo_taxes_tests/transaction/test_transfer_and_swap.py
@@ -17,6 +17,7 @@ def test_transfer():
     wallet_from = WalletFactory.create(fiat=fiat)
     wallet_to = WalletFactory.create(fiat=fiat)
 
+    now = timezone.now()
     # Add some BTC to wallet
     wallet_helper = WalletHelper(wallet_from)
     wallet_helper.deposit(fiat, 1000)
@@ -25,12 +26,14 @@ def test_transfer():
     tx_creator = TransactionCreator()
     tx_creator.add_from_detail(wallet=wallet_from, currency=btc, quantity=10)
     tx_creator.add_to_detail(wallet=wallet_to, currency=btc, quantity=10)
-    tx = tx_creator.create_transfer(timestamp=timezone.now())
+    tx_creator.add_fee_detail(wallet=wallet_to, currency=btc, quantity=1)
+    tx = tx_creator.create_transfer(timestamp=now)
 
     assert tx.transaction_type == TransactionType.TRANSFER
     assert tx.from_detail.cost_basis == tx.to_detail.cost_basis
     assert wallet_from.get_current_balance("BTC") == Decimal(0)
-    assert wallet_to.get_current_balance("BTC") == Decimal(10)
+    assert wallet_to.get_current_balance("BTC") == Decimal(9)
+    assert wallet_to.get_consumable_currency_balances(btc, now)[-1].quantity_left == Decimal(9)
 
 
 @pytest.mark.django_db
@@ -38,12 +41,13 @@ def test_swap():
     fiat = FiatCurrencyFactory.create(symbol="EUR")
     ven = CryptoCurrencyFactory.create(symbol="VEN")
     vet = CryptoCurrencyFactory.create(symbol="VET")
+    btc = CryptoCurrencyFactory.create(symbol="BTC")
 
     wallet = WalletFactory.create(fiat=fiat)
 
     # Add some BTC to wallet
     wallet_helper = WalletHelper(wallet)
-    wallet_helper.deposit(fiat, 1000)
+    wallet_helper.deposit(fiat, 2000)
     wallet_helper.trade(fiat, 1000, ven, 1000)
     wallet_helper.swap(ven, 1000, vet, 100000)
 
@@ -52,6 +56,12 @@ def test_swap():
     assert tx.from_detail.cost_basis == tx.to_detail.cost_basis
     assert wallet.get_current_balance("VEN") == Decimal(0)
     assert wallet.get_current_balance("VET") == Decimal(100000)
+
+    # 1:10 Redenomination
+    wallet_helper.trade(fiat, 1000, btc, 10)
+    wallet_helper.swap(from_currency=btc, from_currency_quantity=10, to_currency=btc, to_currency_quantity=100)
+    balances = wallet.get_current_balance()
+    assert balances["BTC"] == 100
 
 
 # TODO: Test using FIFO to make sure original cost_basis is kept for every single coin
