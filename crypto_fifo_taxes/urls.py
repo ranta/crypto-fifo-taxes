f30152134bbead4d2ec9b56b7c2d7f5d9323d344
Eemeli Ranta <rantaeemeli@gmail.com>
2021-10-27 16:53:05 +0300

    Refactor transaction list view to a class view

--- crypto_fifo_taxes/urls.py
+++ crypto_fifo_taxes/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 from crypto_fifo_taxes.views.graph import snapshot_graph
-from crypto_fifo_taxes.views.transactions import transaction_list
+from crypto_fifo_taxes.views.transactions import TransactionListView
 
 urlpatterns = [
-    path("transactions/", transaction_list),
+    path("transactions/", TransactionListView.as_view()),
     path("graph/", snapshot_graph),
 ]
--- crypto_fifo_taxes/views/transactions.py
+++ crypto_fifo_taxes/views/transactions.py
@@ -1,20 +1,23 @@
 from django.db.models import F, Sum
-from django.http import HttpResponse
-from django.template import loader
+from django.views.generic import ListView
 
 from crypto_fifo_taxes.enums import TransactionLabel
 from crypto_fifo_taxes.models import Transaction
 
 
-def transaction_list(request):
-    transaction_list = (
+class TransactionListView(ListView):
+    model = Transaction
+    queryset = (
         Transaction.objects.annotate(profit=F("gain") - F("fee_amount"))
         .exclude(transaction_label=TransactionLabel.MINING)
         .order_by("timestamp", "pk")
     )
-    totals = transaction_list.aggregate(
-        sum_gain=Sum("gain"), sum_fee_amount=Sum("fee_amount"), sum_profit=Sum("profit")
-    )
-    template = loader.get_template("transactions.html")
-    context = {"transaction_list": transaction_list, "totals": totals}
-    return HttpResponse(template.render(context, request))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["totals"] = self.get_queryset().aggregate(
+            sum_gain=Sum("gain"),
+            sum_fee_amount=Sum("fee_amount"),
+            sum_profit=Sum("profit"),
+        )
+        return context
