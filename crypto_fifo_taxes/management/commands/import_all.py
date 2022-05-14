38aa0fb89365d5683edb0641d161b3558d2000ce
Eemeli Ranta <rantaeemeli@gmail.com>
2022-05-14 17:12:18 +0300

    Add `mode` parameter to `import_all` management command
    It's only passed to binance sync

--- crypto_fifo_taxes/management/commands/import_all.py
+++ crypto_fifo_taxes/management/commands/import_all.py
@@ -6,11 +6,12 @@ from crypto_fifo_taxes.utils.wrappers import print_time_elapsed_new_transactions
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("-d", "--date", type=str, help="Start from this date. Format: YYYY-MM-DD")
+        parser.add_argument("-m", "--mode", type=int, help="Mode this sync will be run in. 0=fast, 1=full")
 
     @print_time_elapsed_new_transactions
     def import_all(self):
         # Import transactions
-        call_command("sync_binance", date=self.date)
+        call_command("sync_binance", date=self.date, mode=self.mode)
         call_command("import_coinbase_json")
         call_command("import_binance_eth2_json")
         call_command("import_nicehash")
@@ -27,5 +28,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self.date = kwargs.pop("date")
+        self.mode = kwargs.pop("mode")
 
         self.import_all()
