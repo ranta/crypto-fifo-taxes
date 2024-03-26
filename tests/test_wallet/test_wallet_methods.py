ranta/crypto-fifo-taxes

commit 4ab13c403708e51b6edbedd12162fa54f02dd77c

Fix `quantity_left` for transactions with identical timestamp
- The quantity of both transactions was included in `quantity_left` for both transactions. This caused the first transaction to have too high `quantity_left`, which caused incorrect value to be returned from the wallet.get_consumable_currency_balances.

crypto_fifo_taxes/models/transaction.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
+import datetime
 from datetime import date
 from decimal import Decimal
-from typing import TYPE_CHECKING, Any, Optional
+from typing import TYPE_CHECKING, Any
 
 from django.conf import settings
 from django.db import models
@@ -77,7 +80,7 @@ class Transaction(models.Model):
 
     @staticmethod
     def _get_detail_cost_basis(
-        transaction_detail: "TransactionDetail", sell_price: Decimal | None = None
+        transaction_detail: TransactionDetail, sell_price: Decimal | None = None
     ) -> tuple[Decimal, bool]:
         """
         Use FIFO to get used currency quantities and cost bases.
@@ -93,7 +96,7 @@ class Transaction(models.Model):
         If it's advantageous to use deemed acquisition cost, it is used
         https://www.vero.fi/henkiloasiakkaat/omaisuus/sijoitukset/osakkeiden_myynt/
         """
-        consumable_balances: list["TransactionDetail"] = transaction_detail.get_consumable_balances()
+        consumable_balances: list[TransactionDetail] = transaction_detail.get_consumable_balances()
         required_quantity: Decimal = transaction_detail.quantity
         cost_bases: list[tuple] = []  # [(quantity, cost_basis)]
         only_hmo_used = True
@@ -122,10 +125,10 @@ class Transaction(models.Model):
                     f"TransactionDetail (id: {balance.id}, {balance}) is missing its `cost_basis`."
                 )
 
-            if required_quantity >= balance.quantity_left:
+            if required_quantity >= balance.quantity:
                 # Fully consume deposit balance
-                cost_bases.append((balance.quantity_left, apply_hmo(balance.cost_basis)))
-                required_quantity -= balance.quantity_left
+                cost_bases.append((balance.quantity, apply_hmo(balance.cost_basis)))
+                required_quantity -= balance.quantity
             else:
                 # Consume only the required quantity
                 cost_bases.append((required_quantity, apply_hmo(balance.cost_basis)))
@@ -134,7 +137,8 @@ class Transaction(models.Model):
         if required_quantity > Decimal(0):
             raise InsufficientFundsError(
                 "Transaction from detail quantity is more than wallet has available to consume! "
-                f"Required: {required_quantity} {transaction_detail.currency}"
+                f"Required: {required_quantity} {transaction_detail.currency}. "
+                f"{transaction_detail.transaction.id=} {transaction_detail.id=}"
             )
 
         sum_quantity = sum(i for i, _ in cost_bases)
@@ -323,7 +327,7 @@ class Transaction(models.Model):
             self.save()
 
     @atomic()
-    def add_detail(self, type: str, wallet: "Wallet", currency: "Currency", quantity: Decimal):
+    def add_detail(self, type: str, wallet: Wallet, currency: Currency, quantity: Decimal):
         assert type in ["from_detail", "to_detail", "fee_detail"]
 
         detail: TransactionDetail | None = getattr(self, type, None)
@@ -449,7 +453,7 @@ class TransactionDetail(models.Model):
         return f"<{self.__class__.__name__} ({self.id}): {detail_type} '{self.currency}' ({self.quantity})>"
 
     @property
-    def transaction(self) -> Optional["Transaction"]:
+    def transaction(self) -> Transaction | None:
         if hasattr(self, "from_detail"):
             return self.from_detail
         elif hasattr(self, "to_detail"):
@@ -458,10 +462,26 @@ class TransactionDetail(models.Model):
             return self.fee_detail
         return None
 
-    def get_consumable_balances(self) -> list["TransactionDetail"]:
+    def get_consumable_balances(self) -> list[TransactionDetail]:
         return self.wallet.get_consumable_currency_balances(
-            self.currency, quantity=self.quantity, timestamp=self.transaction.timestamp
+            currency=self.currency,
+            timestamp=self.transaction.timestamp,
+            quantity=self.quantity,
         )
+
+    def get_last_consumable_balance(self) -> TransactionDetail | None:
+        """Get the balance of this currency after this transaction has been processed."""
+        timestamp = self.transaction.timestamp
+        if hasattr(self, "from_detail"):
+            timestamp += datetime.timedelta(seconds=1)
+
+        last_balance_only = self.wallet.get_consumable_currency_balances(
+            self.currency, quantity=None, timestamp=timestamp
+        )
+
+        if last_balance_only:
+            return last_balance_only[-1]
+        return None
 
     @property
     def total_value(self) -> Decimal | None:

crypto_fifo_taxes/models/wallet.py
@@ -117,6 +117,7 @@ class Wallet(models.Model):
             from_filter |= Q(from_detail__timestamp__lt=timestamp)
             to_filter |= Q(to_detail__timestamp__lte=timestamp)
             fee_filter |= Q(fee_detail__timestamp__lte=timestamp)
+
         total_spent = self.transaction_details.filter(currency=currency).filter(
             (Q(from_detail__isnull=False) & from_filter)
             | (Q(fee_detail__isnull=False) & fee_filter & ~Q(fee_detail__transaction_type=TransactionType.WITHDRAW))
@@ -140,12 +141,17 @@ class Wallet(models.Model):
             f"SELECT * FROM ({sql}) deposits_with_accumed_quantity WHERE accum_quantity > %s",  # noqa: S608,RUF100
             [*params, total_spent],
         )
-
         deposits_filtered = list(deposits_filtered)
+
         if quantity is not None:
             assert quantity > 0
+
+            # Do not use `deposit.quantity_left` as it breaks for transactions with identical timestamps
+            # (Both transactions have the same `accum_quantity`, which includes both transaction quantities).
+            quantity_left = Decimal(0)
             # Return only minimum amount of deposits, exclude all that exceed requested quantity.
             for n, deposit in enumerate(deposits_filtered):
-                if deposit.quantity_left >= quantity:
+                quantity_left += deposit.quantity
+                if quantity_left >= quantity:
                     return deposits_filtered[: n + 1]
         return deposits_filtered

tests/test_wallet/test_wallet_methods.py
@@ -1,3 +1,4 @@
+import datetime
 from decimal import Decimal
 
 import pytest
@@ -82,8 +83,8 @@ def test_get_consumable_currency_balances():
     wallet = WalletFactory.create()
     wallet_helper = WalletHelper(wallet)
 
-    CurrencyPriceFactory.create(currency="BTC", fiat="ADA", date=wallet_helper.date())
-    crypto = CryptoCurrencyFactory.create(symbol="ADA")
+    CurrencyPriceFactory.create(currency="BTC", fiat="EUR", date=wallet_helper.date())
+    crypto = CryptoCurrencyFactory.create(symbol="BTC")
 
     # No deposits, nothing should be returned
     assert len(wallet.get_consumable_currency_balances(crypto)) == 0
@@ -91,28 +92,124 @@ def test_get_consumable_currency_balances():
     # Deposit some cryptocurrency to wallet in two separate events
     wallet_helper.deposit(crypto, quantity=100)
     wallet_helper.deposit(crypto, quantity=50)
+
+    # Current balance = 150
     currencies = wallet.get_consumable_currency_balances(crypto)
     assert len(currencies) == 2
     assert currencies[0].quantity_left == 100
     assert currencies[1].quantity_left == 150
+
     # Test the method with quantity kwarg
-    assert len(wallet.get_consumable_currency_balances(crypto, quantity=99)) == 1
-    assert len(wallet.get_consumable_currency_balances(crypto, quantity=101)) == 2
-    assert len(wallet.get_consumable_currency_balances(crypto, quantity=1000)) == 2
+    currencies = wallet.get_consumable_currency_balances(crypto, quantity=99)
+    assert len(currencies) == 1
+    assert currencies[0].quantity_left == 100
+
+    currencies = wallet.get_consumable_currency_balances(crypto, quantity=101)
+    assert len(currencies) == 2
+    assert currencies[0].quantity_left == 100
+    assert currencies[1].quantity_left == 150
+
+    currencies = wallet.get_consumable_currency_balances(crypto, quantity=1000)
+    assert len(currencies) == 2
+    assert currencies[0].quantity_left == 100
+    assert currencies[1].quantity_left == 150
 
     # Withdraw a part of the funds
     wallet_helper.withdraw(crypto, quantity=20)
+    # Current balance = 130
     currencies = wallet.get_consumable_currency_balances(crypto)
     assert len(currencies) == 2
     assert currencies[0].quantity_left == 80
     assert currencies[1].quantity_left == 130
 
     # Withdraw enough to consume the first deposit and part of the second
+    # Current balance = 30
     wallet_helper.withdraw(crypto, quantity=100)
     currencies = wallet.get_consumable_currency_balances(crypto)
     assert len(currencies) == 1
     assert currencies[0].quantity_left == 30
 
     # Everything is withdrawn, nothing should be returned anymore
+    # Current balance = 0
     wallet_helper.withdraw(crypto, quantity=30)
     assert len(wallet.get_consumable_currency_balances(crypto)) == 0
+
+
+@pytest.mark.django_db()
+def test_get_consumable_currency_balances__insufficient_funds():
+    wallet = WalletFactory.create()
+    wallet_helper = WalletHelper(wallet)
+
+    CurrencyPriceFactory.create(currency="BTC", fiat="EUR", date=wallet_helper.date())
+    crypto = CryptoCurrencyFactory.create(symbol="BTC")
+
+    # Deposit some cryptocurrency to wallet in two separate events
+    wallet_helper.deposit(crypto, quantity=100)
+    wallet_helper.deposit(crypto, quantity=250)
+
+    # Current balance = 350
+    currencies = wallet.get_consumable_currency_balances(crypto)
+    assert len(currencies) == 2
+    assert currencies[0].quantity_left == 100
+    assert currencies[1].quantity_left == 350
+
+    # Withdraw more than should be allowed
+    with pytest.raises(InsufficientFundsError):
+        wallet_helper.withdraw(crypto, quantity=400)
+
+
+@pytest.mark.django_db()
+def test_get_consumable_currency_balances__get_last_consumable_balance():
+    wallet = WalletFactory.create()
+    wallet_helper = WalletHelper(wallet)
+
+    CurrencyPriceFactory.create(currency="BTC", fiat="EUR", date=wallet_helper.date())
+    crypto = CryptoCurrencyFactory.create(symbol="BTC")
+
+    tx_1 = wallet_helper.deposit(crypto, quantity=100)
+    tx_2 = wallet_helper.deposit(crypto, quantity=200)
+    tx_3 = wallet_helper.withdraw(crypto, quantity=150)
+    tx_4 = wallet_helper.deposit(crypto, quantity=100)
+
+    # Current balance = 250
+    currencies = wallet.get_consumable_currency_balances(crypto)
+    assert len(currencies) == 2
+    assert currencies[0].quantity_left == 150
+    assert currencies[1].quantity_left == 250
+
+    assert tx_1.to_detail.get_last_consumable_balance().quantity_left == 100
+    assert tx_2.to_detail.get_last_consumable_balance().quantity_left == 300
+    assert tx_3.from_detail.get_last_consumable_balance().quantity_left == 150
+    assert tx_4.to_detail.get_last_consumable_balance().quantity_left == 250
+
+
+@pytest.mark.django_db()
+def test_get_consumable_currency_balances__same_timestamp():
+    wallet = WalletFactory.create()
+    wallet_helper = WalletHelper(wallet)
+
+    CurrencyPriceFactory.create(currency="BTC", fiat="EUR", date=wallet_helper.date())
+    crypto = CryptoCurrencyFactory.create(symbol="BTC")
+
+    # Deposit some cryptocurrency to wallet in two separate events
+    timestamp = datetime.datetime(2021, 1, 1, 12)
+    tx_1 = wallet_helper.deposit(crypto, quantity=100, timestamp=timestamp)
+    tx_2 = wallet_helper.deposit(crypto, quantity=200, timestamp=timestamp)
+
+    # Current balance = 300
+    currencies = wallet.get_consumable_currency_balances(crypto)
+    assert len(currencies) == 2
+    # Transactions with identical timestamps have duplicate quantity_left
+    assert currencies[0].quantity_left == 300
+    assert currencies[1].quantity_left == 300
+    assert tx_1.to_detail.get_last_consumable_balance().quantity_left == 300
+    assert tx_2.to_detail.get_last_consumable_balance().quantity_left == 300
+
+    currencies = wallet.get_consumable_currency_balances(crypto, quantity=50)
+    assert len(currencies) == 1
+    assert currencies[0].quantity_left == 300
+
+    currencies = wallet.get_consumable_currency_balances(crypto, quantity=150)
+    assert len(currencies) == 2
+    assert currencies[0].quantity_left == 300
+    assert currencies[1].quantity_left == 300

commit 5166872da9cc21cb05847f370d98163ec2f75b9e

Fix using partially consumed deposit balances
- Fix trading from and to the same currency
- This regression was introduced in 4ab13c403708e51b6edbedd12162fa54f02dd77c

crypto_fifo_taxes/migrations/0017_make_tx_timestamp_unique.py
@@ -1,0 +1,30 @@
+# Generated by Django 5.0.2 on 2024-03-26 17:33
+from datetime import timedelta
+
+from django.db import migrations, models
+
+
+def fix_duplicate_timestamps(apps, schema_editor):
+    Transaction = apps.get_model("crypto_fifo_taxes", "Transaction")
+
+    qs = Transaction.objects.values("timestamp").annotate(count=models.Count("timestamp")).filter(count__gt=1)
+    for tx in qs:
+        transactions = Transaction.objects.filter(timestamp=tx["timestamp"])
+        for i, tx in enumerate(transactions):
+            tx.timestamp = tx.timestamp + timedelta(microseconds=i)
+            tx.save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("crypto_fifo_taxes", "0016_remove_unique_together"),
+    ]
+
+    operations = [
+        migrations.RunPython(fix_duplicate_timestamps, reverse_code=migrations.RunPython.noop),
+        migrations.AlterField(
+            model_name="transaction",
+            name="timestamp",
+            field=models.DateTimeField(unique=True),
+        ),
+    ]

crypto_fifo_taxes/models/transaction.py
@@ -38,7 +38,10 @@ class TransactionManager(models.Manager):
 class Transaction(models.Model):
     """Contains static values for a transaction"""
 
-    timestamp = models.DateTimeField()
+    # Unique timestamp is required for each transaction, as
+    # and having multiple transactions with the same timestamp will cause issues
+    # In case of Transactions with the same timestamp, the transactions should be staggered by microseconds.
+    timestamp = models.DateTimeField(unique=True)
     transaction_type = EnumIntegerField(TransactionType, default=TransactionType.UNKNOWN)
     transaction_label = EnumIntegerField(TransactionLabel, default=TransactionLabel.UNKNOWN)
     description = models.TextField(blank=True, default="")
@@ -125,14 +128,19 @@ class Transaction(models.Model):
                     f"TransactionDetail (id: {balance.id}, {balance}) is missing its `cost_basis`."
                 )
 
-            if required_quantity >= balance.quantity:
-                # Fully consume deposit balance
-                cost_bases.append((balance.quantity, apply_hmo(balance.cost_basis)))
-                required_quantity -= balance.quantity
-            else:
-                # Consume only the required quantity
+            # Use the lesser of the two quantities
+            # 1. If the original balance is partially used, use the remaining quantity
+            # 2. Otherwise, use original quantity
+            available_quantity = min(balance.quantity, balance.quantity_left)
+
+            # Deposit has more than enough to cover the transaction, consume only what is needed.
+            if required_quantity <= available_quantity:
                 cost_bases.append((required_quantity, apply_hmo(balance.cost_basis)))
                 required_quantity -= required_quantity
+            # Deposit has less than needed, consume all of it.
+            else:
+                cost_bases.append((available_quantity, apply_hmo(balance.cost_basis)))
+                required_quantity -= available_quantity
 
         if required_quantity > Decimal(0):
             raise InsufficientFundsError(
@@ -472,8 +480,8 @@ class TransactionDetail(models.Model):
     def get_last_consumable_balance(self) -> TransactionDetail | None:
         """Get the balance of this currency after this transaction has been processed."""
         timestamp = self.transaction.timestamp
-        if hasattr(self, "from_detail"):
-            timestamp += datetime.timedelta(seconds=1)
+        # Include self in the query by adding 1 microsecond to the timestamp
+        timestamp += datetime.timedelta(microseconds=1)
 
         last_balance_only = self.wallet.get_consumable_currency_balances(
             self.currency, quantity=None, timestamp=timestamp

crypto_fifo_taxes/models/wallet.py
@@ -121,9 +121,12 @@ class Wallet(models.Model):
             to_filter |= Q(to_detail__timestamp__lte=timestamp)
             fee_filter |= Q(fee_detail__timestamp__lte=timestamp)
 
-        total_spent = self.transaction_details.filter(currency=currency).filter(
-            (Q(from_detail__isnull=False) & from_filter)
-            | (Q(fee_detail__isnull=False) & fee_filter & ~Q(fee_detail__transaction_type=TransactionType.WITHDRAW))
+        total_spent = self.transaction_details.filter(
+            Q(currency=currency)
+            & (
+                (Q(from_detail__isnull=False) & from_filter)
+                | (Q(fee_detail__isnull=False) & fee_filter & ~Q(fee_detail__transaction_type=TransactionType.WITHDRAW))
+            )
         ).aggregate(total_spent=Sum("quantity"))["total_spent"] or Decimal(0)
 
         # All deposits of currency to the wallet, annotated with
@@ -148,13 +151,8 @@ class Wallet(models.Model):
 
         if quantity is not None:
             assert quantity > 0
-
-            # Do not use `deposit.quantity_left` as it breaks for transactions with identical timestamps
-            # (Both transactions have the same `accum_quantity`, which includes both transaction quantities).
-            quantity_left = Decimal(0)
             # Return only minimum amount of deposits, exclude all that exceed requested quantity.
             for n, deposit in enumerate(deposits_filtered):
-                quantity_left += deposit.quantity
-                if quantity_left >= quantity:
+                if deposit.quantity_left >= quantity:
                     return deposits_filtered[: n + 1]
         return deposits_filtered

crypto_fifo_taxes/utils/transaction_creator.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 from decimal import Decimal
 
 from django.db.transaction import atomic
@@ -163,6 +163,14 @@ class TransactionCreator:
             if is_mining:
                 self.transaction_label = TransactionLabel.MINING
 
+    def _get_final_timestamp(self):
+        # Ensure the timestamp is unique by adding milliseconds until it is
+        while True:
+            if not Transaction.objects.filter(timestamp=self.timestamp).exists():
+                break
+            self.timestamp += timedelta(milliseconds=1)
+        return self.timestamp
+
     @atomic()
     def create_transaction(self) -> Transaction:
         assert self.timestamp is not None
@@ -175,7 +183,7 @@ class TransactionCreator:
             detail.save()
 
         transaction = Transaction(
-            timestamp=self.timestamp,
+            timestamp=self._get_final_timestamp(),
             description=self.description,
             tx_id=self.tx_id,
             transaction_type=self.transaction_type,

tests/test_wallet/test_wallet_methods.py
@@ -9,6 +9,7 @@ from crypto_fifo_taxes.models import Currency
 from tests.factories import (
     CryptoCurrencyFactory,
     CurrencyPriceFactory,
+    FiatCurrencyFactory,
     TransactionDetailFactory,
     TransactionFactory,
     WalletFactory,
@@ -188,28 +189,47 @@ def test_get_consumable_currency_balances__same_timestamp():
     wallet = WalletFactory.create()
     wallet_helper = WalletHelper(wallet)
 
-    CurrencyPriceFactory.create(currency="BTC", fiat="EUR", date=wallet_helper.date())
     crypto = CryptoCurrencyFactory.create(symbol="BTC")
+    CurrencyPriceFactory.create(currency=crypto, fiat=wallet.fiat, date=wallet_helper.date())
 
     # Deposit some cryptocurrency to wallet in two separate events
-    timestamp = datetime.datetime(2021, 1, 1, 12)
+    timestamp = datetime.datetime.combine(wallet_helper.date(), datetime.time(12))
     tx_1 = wallet_helper.deposit(crypto, quantity=100, timestamp=timestamp)
     tx_2 = wallet_helper.deposit(crypto, quantity=200, timestamp=timestamp)
+
+    assert tx_1.timestamp == tx_2.timestamp - datetime.timedelta(milliseconds=1)
 
     # Current balance = 300
     currencies = wallet.get_consumable_currency_balances(crypto)
     assert len(currencies) == 2
-    # Transactions with identical timestamps have duplicate quantity_left
-    assert currencies[0].quantity_left == 300
+    assert currencies[0].quantity_left == 100
     assert currencies[1].quantity_left == 300
-    assert tx_1.to_detail.get_last_consumable_balance().quantity_left == 300
+    assert tx_1.to_detail.get_last_consumable_balance().quantity_left == 100
     assert tx_2.to_detail.get_last_consumable_balance().quantity_left == 300
 
     currencies = wallet.get_consumable_currency_balances(crypto, quantity=50)
     assert len(currencies) == 1
-    assert currencies[0].quantity_left == 300
+    assert currencies[0].quantity_left == 100
 
     currencies = wallet.get_consumable_currency_balances(crypto, quantity=150)
     assert len(currencies) == 2
-    assert currencies[0].quantity_left == 300
+    assert currencies[0].quantity_left == 100
     assert currencies[1].quantity_left == 300
+
+
+@pytest.mark.django_db()
+def test_same_from_to_symbol():
+    fiat = FiatCurrencyFactory.create(symbol="EUR")
+    crypto = CryptoCurrencyFactory.create(symbol="BTC")
+
+    wallet = WalletFactory.create(fiat=fiat)
+    wallet_helper = WalletHelper(wallet)
+    CurrencyPriceFactory.create(currency=crypto, fiat=fiat, date=wallet_helper.date(), price=1000)
+
+    tx_1 = wallet_helper.deposit(crypto, 1000)
+    tx_2 = wallet_helper.trade(crypto, 1000, crypto, 500)
+
+    assert wallet.get_current_balance(crypto.symbol) == 500
+
+    assert tx_1.to_detail.get_last_consumable_balance().quantity_left == 1000
+    assert tx_2.to_detail.get_last_consumable_balance().quantity_left == 500