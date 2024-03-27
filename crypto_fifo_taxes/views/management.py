ae9ba079bb6579b25eea72e4c8f95da65106380f
Eemeli Ranta <rantaeemeli@gmail.com>
2024-03-27 02:03:06 +0200

    Add `Unprocessed Transactions` to Management view

--- crypto_fifo_taxes/views/management.py
+++ crypto_fifo_taxes/views/management.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from crypto_fifo_taxes.models import Snapshot, Transaction
+from crypto_fifo_taxes.models import CurrencyPrice, Snapshot, Transaction
 
 
 class ManagementView(TemplateView):
@@ -9,21 +9,15 @@ class ManagementView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        latest_tx = Transaction.objects.order_by("-timestamp").first()
-        if latest_tx is not None:
-            context["latest_transaction_timestamp"] = str(latest_tx.timestamp)
+        if latest_tx := Transaction.objects.order_by("-timestamp").first():
+            context["latest_transaction_timestamp"] = latest_tx.timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
-            if latest_tx.from_detail is not None:
-                latest_currency = latest_tx.from_detail.currency
-            else:
-                latest_currency = latest_tx.to_detail.currency
+        if latest_price := CurrencyPrice.objects.order_by("-date").first():
+            context["latest_currency_price_date"] = str(latest_price.date)
 
-            latest_price = latest_currency.prices.order_by("-date").first()
-            if latest_price is not None:
-                context["latest_currency_price_date"] = str(latest_price.date)
-
-        latest_snapshot = Snapshot.objects.order_by("-date").first()
-        if latest_snapshot is not None:
+        if latest_snapshot := Snapshot.objects.order_by("-date").first():
             context["latest_snapshot_timestamp"] = str(latest_snapshot.date)
+
+        context["unprocessed_transactions_count"] = Transaction.objects.filter(gain=None).count()
 
         return context
